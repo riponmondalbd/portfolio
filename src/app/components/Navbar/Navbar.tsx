dc1bd6c7fe3c0f8c91bbf285bbc9f49025b3f7ef
riponmondalbd <riponmondal149@gmail.com>
2025-07-02 01:59:50 +0600

    make side logo

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../../../public/logo-orange.png";
+
 export default function Navbar() {
   return (
-    <div>
-      <h1>Hello from navbar</h1>
+    <div className="bg-[#f4f4f4]">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between">
+          <Link href={"/"}>
+            <div className="flex items-center">
+              <Image
+                src={logo}
+                height={17}
+                width={17}
+                alt="main logo"
+                className="mt-1"
+              />
+              <span className="text-4xl font-bold uppercase">ipon</span>
+            </div>
+          </Link>
+          <div>Menu bar</div>
+          <div>Contact</div>
+        </div>
+      </div>
     </div>
   );
 }
